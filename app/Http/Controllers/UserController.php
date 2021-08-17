5401d00bd4803de4ec3867350fbdb64a4819acc4
anhtu123qt <anhtu123qt@gmail.com>
2021-08-17 11:49:06 +0700

    register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,9 @@ class UserController extends Controller
         return response()->json(['data' => $users],200);
 
     }
+    public function register() {
+
+    }
 
     /**
      * Show the form for creating a new resource.
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('user',UserController::class);
+Route::post('register','UserController@register');
 
